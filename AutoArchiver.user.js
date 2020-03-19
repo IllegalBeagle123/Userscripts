e72c00f0aadbae39e85ce8def9d2a8c33c43eebf
IllegalBeagle123 <39150378+IllegalBeagle123@users.noreply.github.com>
2020-03-19 11:32:22 +1100

    Update Auto Archiver
    Now supports restricted sites.

--- AutoArchiver.user.js
+++ AutoArchiver.user.js
@@ -1,23 +1,33 @@
 // ==UserScript==
 // @name         Auto Archiver
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  Automatically saves blocked sites with web.archive.com.
 // @author       Dylan Barrett
-// @match        http://*/internetaccessnotice*.html*
-// @match        https://web.archive.org/web/*/*/
+// @match        *fdc-edugw-cf04.ed.act.edu.au/*
+// @match        *web.archive.org/web/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const url = urlParams.get('URL')
-    if(urlParams.get('URL') != null){
-        window.location.replace("https://web.archive.org/save/" + url)
+    try{
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const url = urlParams.get('URL')
+        if(urlParams.get('URL') != null){
+            window.location.replace("https://web.archive.org/save/" + url)
+        }
+        if(document.getElementById("error") != null){
+            window.location.replace("https://web.archive.org/save/" + document.getElementsByName("url_preload")[0].value)
+        }
+        if(document.forms[0] != undefined){
+            window.location.replace("https://web.archive.org/save/" + document.forms[0].INPUT_URL_AUTH.value)
+        }
     }
-    if(document.getElementById("error")!=null){
-        window.location.replace("https://web.archive.org/save/" + document.getElementsByName("url_preload")[0].value)
+    catch(error){
+        if(error != "TypeError: Cannot read property 'value' of undefined"){
+            alert("An error has occured: " + error)
+        }
     }
 })();
